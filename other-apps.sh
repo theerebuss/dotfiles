5bcd3fffaaca42960d42b3d9b7d1cdde696da65e
theerebuss <39736248+andrefcdias@users.noreply.github.com>
2025-06-24 10:57:27 +0200

    add other useful apps

---
+++ other-apps.sh
@@ -0,0 +1,11 @@
+brew install slack
+brew install notion
+brew install 1password
+brew install firefox microssoft-edge google-chrome
+brew install linear-linear
+brew install figma
+brew install cap
+brew install postgresql
+brew install google-cloud-sdk
+brew install --cask docker
+brew install docker-compose
