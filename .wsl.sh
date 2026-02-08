ecca74f1dd257bb29e7d6c9bc8bdc3a404ecfa40
André Dias <39736248+andrefcdias@users.noreply.github.com>
2026-02-08 20:42:35 +0100

    chore(wsl): reduce dupe code

--- .wsl.sh
+++ .wsl.sh
@@ -20,14 +20,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 	sudo apt install gh -y
 gh auth login -s user
 
-git clone --depth=1 https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k ${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/powerlevel10k
-
-CURR_DIR=$(pwd)
-cp -f $CURR_DIR/configs/.zshrc $HOME/.zshrc
-cp -f $CURR_DIR/configs/.p10k.zsh $HOME/.p10k.zsh
+./scripts/install.sh
 
 # Clipboard copy tool
 sudo apt-get install xclip
